56454c1408a088f042e4c735bd7fad3f8ec4de52
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-02 22:31:11 +0900

    Update network3-proxy.sh

--- network3-proxy.sh
+++ network3-proxy.sh
@@ -155,7 +155,6 @@ EOF
     
     # 웹계정과 연동
     URL="https://account.network3.ai/main?o=${IP_ADDRESS}:8080"
-    echo "You can access the dashboard by opening https://account.network3.ai/main?o=$(hostname -I | awk '{print $1}'):$PORT in Chrome." >&2
     echo -e "${GREEN}웹계정과 연동을 진행합니다.${NC}"
     echo -e "${YELLOW}다음 URL로 접속하세요: ${URL}${NC}"
     echo -e "${YELLOW}1. 좌측 상단의 Login버튼을 누르고 이메일 계정으로 로그인을 진행하세요.${NC}"
